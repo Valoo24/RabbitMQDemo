d58f5f57820b19038b51366a0e874115c3e9ac69
Benjamin Malemprée <benjamin_malempree@hotmail.com>
2024-03-08 13:26:07 +0100

    Wrote a program to the sender project to send the message in RabbitMQ. Test OK

--- RabbitMQDemo.Sender/Program.cs
+++ RabbitMQDemo.Sender/Program.cs
@@ -1,2 +1,31 @@
-﻿Console.WriteLine("Welcome to the RabbitMQ Demo by Benjamin Malemprée.");
+﻿using RabbitMQDemo.Core.Abstracts;
+using RabbitMQDemo.Core.Models;
+using RabbitMQDemo.Core.Tools;
+
+IMessage message = new Message();
+RabbitMQManager messageManager = new();
+
+Console.WriteLine("Welcome to the RabbitMQ Demo by Benjamin Malemprée.");
 Console.WriteLine("This window shows the sender client.");
+Console.ReadLine();
+
+Console.Clear();
+Console.WriteLine("Please type the message to send then press \'Enter\'");
+string result = Console.ReadLine();
+if (string.IsNullOrEmpty(result))
+    result = "This is a placeholder message, because user didn't type anything.";
+
+message.Content = result;
+
+Console.Clear();
+try
+{
+    messageManager.SendMessage(message);
+    Console.WriteLine("The Message was sent succesfully.");
+}
+catch(Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
+Console.ReadLine();
