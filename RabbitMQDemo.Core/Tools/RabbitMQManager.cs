8108f6495d20dbb3a4b92bf525da6aa869e138f8
Benjamin Malemprée <benjamin_malempree@hotmail.com>
2024-03-08 11:56:30 +0100

    Added a RabbitMQManager class with a method to send a message

---
+++ RabbitMQDemo.Core/Tools/RabbitMQManager.cs
@@ -0,0 +1,49 @@
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using RabbitMQDemo.Core.Abstracts;
+using System.Text;
+
+namespace RabbitMQDemo.Core.Tools;
+
+public class RabbitMQManager : IDisposable
+{
+    private readonly IModel channel;
+    public RabbitMQManager()
+    {
+        var connectionFactory = new ConnectionFactory()
+        {
+            Uri = new("amqp://guest:guest@localhost:5672")
+        };
+        var connection = connectionFactory.CreateConnection();
+
+        channel = connection.CreateModel();
+    }
+
+    public void SendMessage(IMessage message)
+    {
+
+        channel.ExchangeDeclare(exchange: "demoexchange", type: ExchangeType.Direct);
+        channel.QueueDeclare(queue: "demoqueue", durable: true, exclusive: false, autoDelete: false, arguments: null);
+        channel.QueueBind(queue: "demoqueue", exchange: "demoexchange", routingKey: "");
+
+        var jsonMessage = JsonConvert.SerializeObject(message);
+
+        var messageproperties = channel.CreateBasicProperties();
+        messageproperties.ContentType = "application/json";
+
+        try
+        {
+            channel.BasicPublish(exchange: "demoexchange", routingKey: "",
+                basicProperties: messageproperties, body: Encoding.UTF8.GetBytes(jsonMessage));
+        }
+        catch
+        {
+            throw;
+        }
+    }
+
+    public void Dispose()
+    {
+        if (!channel.IsClosed) channel.Close();
+    }
+}
